caf2cb09c3e8380e598c75ac187f30fe3aecedf6
Kunjesh Mahajan <kunmva@gmail.com>
2022-03-14 10:02:42 +0530

    hosting

---
+++ app.js
@@ -0,0 +1,61 @@
+const cafeList = document.querySelector('#cafe-list')
+const form = document.querySelector("#add-cafe-form")
+function renderCafe(doc){
+
+    let li = document.createElement('li');
+    let name = document.createElement('span')
+    let city = document.createElement('span')
+    let cross = document.createElement('div')
+
+    li.setAttribute('data-id',doc.id)
+    name.textContent = doc.data().name
+    city.textContent = doc.data().city
+    cross.textContent = 'x'
+
+    li.appendChild(name)
+    li.appendChild(city)
+    li.appendChild(cross)
+
+    cafeList.appendChild(li)
+
+    cross.addEventListener('click',(e)=>{
+        e.stopPropagation()
+        let id = e.target.parentElement.getAttribute('data-id')
+        db.collection('cafes').doc(id).delete()
+
+    })
+
+}
+
+db.collection('cafes').orderBy('name').get()
+  .then((snapshot)=>{
+      snapshot.docs.forEach(doc=>{
+          renderCafe(doc)
+      })
+  })
+
+
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+    db.collection('cafes').add({
+        name: form.name.value,
+        city: form.city.value
+    })
+    form.name.value = '',
+    form.city.value= ''
+
+})
+
+db.collection('cafes').orderBy('city').onSnapshot(snapshot =>{
+    let changes = snapshot.docChanges()
+    changes.forEach(change =>{
+        if(change.type == 'added'){
+            renderCafe(change.doc)
+        }else if(change.type == 'removed'){
+            let li = cafeList.querySelector('[data-id=' + change.doc.id + ']')
+            cafeList.removeChild(li)
+        }
+    })
+})
+
